fc87fd7ef50bf7df5671d31bbcc12bbcd30ee64e
Sarmistha Das <sarmistha.das.se@gmail.com>
2025-02-25 11:14:12 +0530

    Render Message and message input, sending and receiving message

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,73 @@
+import ConversationHeader from "@/Components/App/ConversationHeader";
+import MessageInput from "@/Components/App/MessageInput";
+import MessageItem from "@/Components/App/MessageItem";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import ChatLayout from "@/Layouts/ChatLayout";
+import { ChatBubbleLeftIcon } from "@heroicons/react/24/solid";
+import { useEffect, useRef, useState } from "react";
 
-function Home() {
-    return <>Messages</>;
+function Home({ selectedConversation = null, messages = null }) {
+    const [localMessages, setLocalMessages] = useState([]);
+    const messagesCtrRef = useRef(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (messagesCtrRef.current) {
+                messagesCtrRef.current.scrollTop =
+                    messagesCtrRef.current.scrollHeight;
+            }
+        }, 10);
+    }, [selectedConversation]);
+
+    useEffect(() => {
+        setLocalMessages(messages ? messages.data.reverse() : []);
+    }, [messages]);
+
+    return (
+        <>
+            {!messages && (
+                <div className="flex flex-col gap-8 justify-center items-center text-center h-full opacity-35">
+                    <div className="text-2xl md:text-4xl p-16 text-slate-200">
+                        Please select conversation to see messages
+                    </div>
+                    <ChatBubbleLeftIcon />
+                </div>
+            )}
+            {messages && (
+                <>
+                    <ConversationHeader
+                        selectedConversation={selectedConversation}
+                    />
+
+                    <div
+                        ref={messagesCtrRef}
+                        className="flex-1 overflow-y-auto p-5"
+                    >
+                        {}
+                        {localMessages.length === 0 && (
+                            <div className="flex justify-center items-center h-full">
+                                <div className="text-lg text-slate-200">
+                                    No messages found
+                                </div>
+                            </div>
+                        )}
+
+                        {localMessages.length > 0 && (
+                            <div className="flex-1 flex flex-col">
+                                {localMessages.map((message) => (
+                                    <MessageItem
+                                        key={message.id}
+                                        message={message}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <MessageInput conversation={selectedConversation} />
+                </>
+            )}
+        </>
+    );
 }
 
 Home.layout = (page) => {
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html
     lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    data-theme="cupcake"
     class="dark overflow-hidden h-screen"
 >
     <head>
